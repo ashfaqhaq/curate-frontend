24c4b15fd25154b1832ed4ac80aebad6f94f95c7
ashfaqhaq <ashfaq.28010@gmail.com>
2021-08-29 03:41:12 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Auth0Provider} from '@auth0/auth0-react'
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+        domain='dev-5torqz8e.us.auth0.com'
+        clientId='128wykGVXmyeRPfFARUE7OUgLaqpTcwf'
+        redirectUri={window.location.origin}
+        audience="https://dev-5torqz8e.us.auth0.com/api/v2/"
+        scope='read:current_user update:current_user_metadata'
+    >
     <App />
-  </React.StrictMode>,
+    </Auth0Provider>,
   document.getElementById('root')
 );
 
